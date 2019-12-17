487536c4ac89200d41d7a0bc096fef4955420dcc
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-17 21:45:25 +0200

    veikia gudriai

---
+++ Savarankiskas1/Savarankiskas1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grupesNariuKiekis = 0;
+
+            Console.WriteLine("Ivesti grupes nariu kieki:");
+            if (!int.TryParse(Console.ReadLine(), out grupesNariuKiekis))
+            {
+                Console.WriteLine("Negaliu konvertuoti");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            //grupesNariuKiekis = int.Parse(Console.ReadLine());
+
+            // grupesNariuKiekis = Convert.ToInt32(Console.ReadLine());
+
+            if (grupesNariuKiekis == 1)
+            {
+                Console.WriteLine("Tai solo atlikejas");
+            }
+            else if (grupesNariuKiekis == 2)
+            {
+                Console.WriteLine("tai duetas");
+            }
+            else if ((grupesNariuKiekis > 2) && (grupesNariuKiekis < 10))
+            {
+                Console.WriteLine("tai ansamblis");
+            }
+            else if (grupesNariuKiekis >= 10)
+            {
+                Console.WriteLine("tai kamerinis ansamblis");
+            }
+            else
+            {
+                Console.WriteLine("nariu skaicius negalimas: {0}", grupesNariuKiekis);
+            }
+
+            Console.ReadLine();
+
+                
+            
+
+
+        }
+    }
+}
