16fb64e5ef68548d8aeef18006503e9359b05efe
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-17 19:22:44 +0200

    veikia

---
+++ 4/4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1234.7;
+            int a;
+
+            //a = x;
+            a = (int)x;
+            Console.WriteLine(x);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
