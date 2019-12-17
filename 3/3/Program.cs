3c79b8deb3b8ef5b5cb9317a97edc7ff44d69032
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-17 19:12:08 +0200

    veikia

---
+++ 3/3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Human
+    {
+        public string Name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+            human1.Name = "Batman";
+
+            Human human2 = human1;
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            human1.Name = "Joker";
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
