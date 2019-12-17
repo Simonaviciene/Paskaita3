57a6c5af7818ae0e6bf7cb52330b3fec93562a29
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-17 18:45:41 +0200

    veikia

---
+++ 2/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal skaicius1 = 0.1m * 10m;
+            decimal skaicius2 = 0m;
+
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+                skaicius2 += 0.1m;
+            }
+
+            Console.WriteLine("{0} = {1} : {2}", skaicius1, skaicius2, skaicius1 == skaicius2);
+            Console.ReadLine();
+
+        }
+    }
+}
