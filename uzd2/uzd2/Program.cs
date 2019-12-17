1a086005f47825c41567d12a9dab63ddd9a6ddbb
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-17 20:24:25 +0200

    veikia

---
+++ uzd2/uzd2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double atstumas;
+            double laikasSekundemis;
+            double greitis;
+            double km;
+            double valandos;
+
+            Console.WriteLine("iveskite atstuma (metrais): ");
+
+            atstumas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Iveskite Laika (sekundemis): ");
+            laikasSekundemis = Convert.ToInt32(Console.ReadLine());
+            km = atstumas / 1000;
+            valandos = laikasSekundemis / (60d * 60d);
+            greitis = km / valandos;
+            Console.WriteLine("greitis: {0}", greitis );
+
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
